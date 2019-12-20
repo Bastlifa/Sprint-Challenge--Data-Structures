f648480d891d9653c3d41938c4c0137ddec35a55
Eli Sacks <bastlifa@gmail.com>
2019-12-20 12:18:35 -0500

    names runs in under a second

--- names/names.py
+++ names/names.py
@@ -1,5 +1,42 @@
 import time
 
+# Inserting n elements in BST is... nlog(n)
+# Using BST for log_2(n) search times.
+# Searching n elements will be nlog(n)
+# Runtime = 2nlog(n) = nlog(n)
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value >= self.value:
+            if self.right is not None:
+                self.right.insert(value)
+            else:
+                self.right = BinarySearchTree(value)
+        else:
+            if self.left is not None:
+                self.left.insert(value)
+            else:
+                self.left = BinarySearchTree(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if self.value == target:
+            return True
+        elif target > self.value:
+            if self.right is not None:
+                return self.right.contains(target)
+            else: return False
+        elif target < self.value:
+            if self.left is not None:
+                return self.left.contains(target)
+            else: return False
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -16,13 +53,15 @@ duplicates = []
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-names_1.sort()
-names_2.sort()
 
-i,j = 0,0
+name_tree = BinarySearchTree(names_1[0])
 
-while i < 10000 and j < 10000:
+for i in range(1, len(names_1)):
+    name_tree.insert(names_1[i])
     
+for n in names_2:
+    if name_tree.contains(n):
+        duplicates.append(n)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
